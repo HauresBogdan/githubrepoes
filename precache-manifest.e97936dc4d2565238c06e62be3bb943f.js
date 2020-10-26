1395cd28f3db44cda4bd80a7a1e9bc948428b12e
HauresBogdan <hauresbogdan@gmail.com>
2020-10-26 10:42:00 +0200

    Updates

---
+++ precache-manifest.e97936dc4d2565238c06e62be3bb943f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e2700f128f612aa6c7a4ff5dc69792",
+    "url": "/githubrepoes/index.html"
+  },
+  {
+    "revision": "4d0fbcf19ed04cb450d8",
+    "url": "/githubrepoes/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "6bbfbe96d01540d9e9d1",
+    "url": "/githubrepoes/static/css/main.d1ada218.chunk.css"
+  },
+  {
+    "revision": "4d0fbcf19ed04cb450d8",
+    "url": "/githubrepoes/static/js/2.a130bd9a.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/githubrepoes/static/js/2.a130bd9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bbfbe96d01540d9e9d1",
+    "url": "/githubrepoes/static/js/main.606cee32.chunk.js"
+  },
+  {
+    "revision": "8ad8dfa4d392a05343c5",
+    "url": "/githubrepoes/static/js/runtime-main.c049c3fd.js"
+  }
+]);
